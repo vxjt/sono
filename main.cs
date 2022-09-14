71353f137ea5cfb04cd1329cd87716ba36b374c7
vxjt <nsandborn@gmail.com>
2022-09-14 12:00:29 -0500

    added hp
    i hope

--- main.cs
+++ main.cs
@@ -61,25 +61,27 @@ namespace sono
         private void OnUpdate(object s, float deltaTime)
         {
             _tick = _tick + 1;
-            Vector3 testa = new Vector3(0, DynelManager.LocalPlayer.GetAttackRange() / 50, 0);
-            //Debug.DrawSphere(DynelManager.LocalPlayer.Position + testa, 0.3f, DebuggingColor.LightBlue);
             if (sonoWin != null && sonoWin.IsValid && Targeting.TargetChar != null)
             {
                 if (sonoWin.FindView("tText1", out TextView tText1))
                 {
-                    string stat1 = Targeting.TargetChar.GetStat(Stat.AttackCount).ToString();
-                    string stat2 = Targeting.TargetChar.GetStat(Stat.Can).ToString();
-                    string stat4 = Targeting.TargetChar.GetStat((Stat)201).ToString();
+                    string stat1 = Targeting.TargetChar.GetStat(Stat.Height).ToString();
+                    string stat2 = Targeting.TargetChar.Radius.ToString();
+                    string stat4 = Targeting.TargetChar.GetStat((Stat)889).ToString();
+                    float stata = Targeting.TargetChar.GetStat(Stat.PercentRemainingHealth);
+
                     
-                    tText1.Text = $"attack: {stat1} Can: {stat2} aggro: {stat4} flags: {Targeting.TargetChar.GetStat((Stat)0).ToString()}";
+                    tText1.Text = $"{stata}";
                 }
                 if (sonoWin.FindView("tText2", out TextView tText2))
                 {
                     string _statmd = Targeting.TargetChar.GetStat(Stat.MonsterData).ToString();
                     string _statrs = Targeting.TargetChar.Runspeed.ToString();
                     string _statradius = Targeting.TargetChar.GetStat(Stat.CharRadius).ToString();
-                    string _statscale = Targeting.TargetChar.GetStat(Stat.Scale).ToString();
-                    tText2.Text = $"md: {_statmd} rs: {_statrs} radius: {_statradius} scale: {_statscale}";
+                    string _statradius2 = Targeting.TargetChar.Radius.ToString();
+                    string _statdist = Targeting.TargetChar.DistanceFrom(DynelManager.LocalPlayer).ToString();
+                    string _logidist = Targeting.TargetChar.GetLogicalRangeToTarget(DynelManager.LocalPlayer).ToString();
+                    tText2.Text = $"md: {_statmd} rs: {_statrs} radius: {_statradius} radius2: {_statradius2} _statdist {_statdist} logic: {_logidist}";
                 }
                 if (sonoWin.FindView("pbhealth", out PowerBarView pbHealth))
                 {
@@ -110,38 +112,54 @@ namespace sono
             {
                 if (Settings["Vision"].AsBool())
                 {
-                    float _size = character.GetStat(Stat.Scale) / 100 * character.GetStat(Stat.CharRadius);
+                    Vector3 _pathColor = v3Web(255, 185, 127);
+                    Vector3 _radarColor = v3Web(97, 195, 195);
+                    Vector3 _healthColor = v3Web(255, 61, 61);
+                    Vector3 _emptyColor = v3Web(255, 214, 214);
+                    float _hpperc = character.GetStat(Stat.PercentRemainingHealth) / 100;
+                    float _range = 20 - DynelManager.LocalPlayer.Radius + character.Radius;
+                    float _health = character.GetStat(Stat.Health);
+                    float _maxhealth = character.GetStat(Stat.MaxHealth);
+                    float _healthpc = Math.Max(0, _health / _maxhealth);
+                    Vector3 _anchor = new Vector3(0, character.Radius * 7, 0);
                     Vector3 v3Up = new Vector3(0, 1, 0);
-                    Vector3 v3yscale = new Vector3(0, _size, 0);
-                    Vector3 v3Shift = new Vector3(_size * -1f, 0, 0);
                     Vector3 v3Delta = DynelManager.LocalPlayer.Position - character.Position;
-                    double dubLength = Math.Sqrt(v3Delta.X * v3Delta.X + v3Delta.Y * v3Delta.Y + v3Delta.Z * v3Delta.Z);
-                    Vector3 v3Norm = new Vector3(v3Delta.X / dubLength, v3Delta.Y / dubLength, v3Delta.Z / dubLength);
+                    Vector3 v3Norm = new Vector3(v3Delta.X / character.DistanceFrom(DynelManager.LocalPlayer), v3Delta.Y / character.DistanceFrom(DynelManager.LocalPlayer), v3Delta.Z / character.DistanceFrom(DynelManager.LocalPlayer));
+                    Debug.DrawSphere(DynelManager.LocalPlayer.Position, DynelManager.LocalPlayer.Radius, _pathColor);
                     Vector3 v3X = Vector3.Cross(v3Up, v3Norm);
-
-                    //Debug.DrawLine(character.Position + v3Scale, character.Position + v3Norm * character.GetStat(Stat.CharRadius) + v3Scale, DebuggingColor.Blue);
-                    //Debug.DrawLine(character.Position + v3Scale, character.Position + v3Up * character.GetStat(Stat.CharRadius) + v3Scale, DebuggingColor.Green);
-                    //Debug.DrawLine(character.Position + v3Scale, character.Position + v3X * character.GetStat(Stat.CharRadius) + v3Scale, DebuggingColor.Yellow);
-                    if(!character.IsPlayer && character != DynelManager.LocalPlayer && !character.IsAttacking) {
-                        Debug.DrawLine(character.Position, character.Position + v3Norm * 20f, DebuggingColor.Red);
-                    }
-                    //Debug.DrawLine(Aggro(character, DynelManager.LocalPlayer));
-                    //float _scalex
-                    //float _scaley = 0.67f;
-                    //float _width
-                    //float _height
-                    //Debug.DrawLine(character.Position + v3Scale * 1.05f - v3X * character.GetStat(Stat.CharRadius) * 0.9f, character.Position + v3Scale * 1.05f - v3X * character.GetStat(Stat.CharRadius) * 0.2f, DebuggingColor.Green);
-                    //Debug.DrawLine(character.Position + v3Scale - v3X * character.GetStat(Stat.CharRadius) * 1.05f, character.Position + v3Scale - v3X * character.GetStat(Stat.CharRadius) * 1.05f + v3Up * 0.2f, DebuggingColor.Yellow);
-                    //Debug.DrawLine(character.Position + v3Scale - v3X * character.GetStat(Stat.CharRadius) * 0.2f, character.Position + v3Scale - v3X * character.GetStat(Stat.CharRadius) * 0.2f + v3Up * 0.2f, DebuggingColor.Blue);
-                    if (character.IsPathing)
+                    //Debug.DrawLine(character.Position, character.Position + v3Norm * character.GetStat(Stat.CharRadius), DebuggingColor.Blue);
+                    //Debug.DrawLine(character.Position, character.Position + v3Up, DebuggingColor.Green);
+                    //Debug.DrawLine(character.Position, character.Position + v3X * character.GetStat(Stat.CharRadius), DebuggingColor.Yellow);
+                    //low horiz
+                    Debug.DrawLine(character.Position + _anchor - v3X * character.Radius * 7 * _healthpc - v3X * character.Radius * 2, character.Position + _anchor - v3X * character.Radius * 9, _emptyColor);
+                    Debug.DrawLine(character.Position + _anchor - v3X * character.Radius * 2, character.Position + _anchor - v3X * character.Radius * 7 * _healthpc - v3X * character.Radius * 2, _healthColor);
+                    
+                    //high horiz
+                    Debug.DrawLine(character.Position + _anchor + new Vector3(0, character.Radius / 2, 0) - v3X * character.Radius * 2, character.Position + _anchor + new Vector3(0, character.Radius / 2, 0) - v3X * character.Radius * 7 * _healthpc - v3X * character.Radius * 2, _healthColor);
+                    Debug.DrawLine(character.Position + _anchor + new Vector3(0, character.Radius / 2, 0) - v3X * character.Radius * 7 * _healthpc - v3X * character.Radius * 2, character.Position + _anchor + new Vector3(0, character.Radius / 2, 0) - v3X * character.Radius * 9, _emptyColor);
+                    //left vert
+                    Debug.DrawLine(character.Position + _anchor - v3X * character.Radius * 2, character.Position + _anchor + new Vector3(0, character.Radius / 2, 0) - v3X * character.Radius * 2, _healthColor);
+                    //right verts
+                    Debug.DrawLine(character.Position + _anchor - v3X * character.Radius * 9, character.Position + _anchor + new Vector3(0, character.Radius / 2, 0) - v3X * character.Radius * 9, _emptyColor);   
+                    Debug.DrawLine(character.Position + _anchor - v3X * character.Radius * 7 * _healthpc - v3X * character.Radius * 2, character.Position + _anchor + new Vector3(0, character.Radius / 2, 0) - v3X * character.Radius * 7 * _healthpc - v3X * character.Radius * 2, _healthColor);
+               
+                    //Debug.DrawLine(character.Position + _anchor - v3X * character.Radius * 2, character.Position + _anchor - v3X * character.Radius * (7 + 2), _emptyColor);
+                    if(character.IsNpc && !character.IsAttacking)
                     {
-                        Debug.DrawLine(character.Position, character.PathingDestination + v3Up * Convert.ToSingle(Math.Sin(_tick * character.Runspeed / 500)) * 0.2f, DebuggingColor.LightBlue);
-                        Debug.DrawSphere(character.PathingDestination + v3Up * Convert.ToSingle(Math.Sin(_tick * character.Runspeed / 500)) * 0.2f, 0.3f, DebuggingColor.LightBlue);
+                        Debug.DrawLine(character.Position, character.DistanceFrom(DynelManager.LocalPlayer) < _range ? DynelManager.LocalPlayer.Position : character.Position + v3Norm * _range, _radarColor);
+                    }
+                     if (character.IsPathing)
+                    {
+                        Debug.DrawLine(character.Position, character.PathingDestination, _pathColor);
+                        Debug.DrawSphere(character.PathingDestination + v3Up * (Convert.ToSingle(Math.Sin(_tick * character.Runspeed / 1000)) / 2f + 1.5f), 0.4f, _pathColor);
                     }
                 }
             }
         }
-
+        private Vector3 v3Web(int r, int g, int b)
+        {
+            return new Vector3((float)r / 255, (float)g / 255, (float)b / 255);
+        }
         public override void Teardown()
         {
             Chat.WriteLine("Teardown time!");
